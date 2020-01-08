4db005b2ad969ad33593cdc1aff3d4950c7206a5
Miles Higbie <miles.higbie@gmail.com>
2020-01-08 18:44:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-<<<<<<< HEAD
+
     self.end_with?(".")
   end
 
@@ -18,43 +18,5 @@ class String
   def count_sentences
     self.split(/[.?!]+/).count 
   end
-
-=======
-    if self.end_with?(".")
-      return true 
-    else 
-      return false
-    end 
-  end
-
-  def question?
-     if self.end_with?("?")
-      return true 
-    else 
-      return false
-    end 
-  end
-
-  def exclamation?
-   if self.end_with?("!")
-      return true 
-    else 
-      return false
-    end 
-  end
-
-  def count_sentences
-  string_counter = []
-    if self.sentence? 
-      string_counter.push(self.split("."))
-    elsif self.question?
-      string_counter.push(self.split("?"))
-    elsif self.exclamation? 
-      string_counter.push(self.split("!"))
-    end 
-    binding.pry
-    string_counter.flatten.join("").split(".").count
-  end 
   
->>>>>>> 0681357c8b5f3d96ffcdccc0bca744d9339f8f9f
-end 
+end
